fa0c249a1a010dc4392b553dea0f466cc987ae8e
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-08-06 09:58:16 -0300

    commit inicial

---
+++ src/test/java/com/entra21/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entra21.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
