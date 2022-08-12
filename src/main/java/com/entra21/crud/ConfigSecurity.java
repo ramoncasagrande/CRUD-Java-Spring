7614ccd634ab25e6d625ff02bd7acc80ccbc2d3d
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-08-12 20:29:26 -0300

    Sobrescrito o método UserDetailsService

--- src/main/java/com/entra21/crud/ConfigSecurity.java
+++ src/main/java/com/entra21/crud/ConfigSecurity.java
@@ -1,9 +1,14 @@
 package com.entra21.crud;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 @Configuration      //indica que a classe fonecerá configurações para o springboot
@@ -20,7 +25,17 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
                 .permitAll()                    //Permite acesso ao fomulario de login a todos
                 .and()
             .logout()
-                .permitAll();                   ////Permite acesso ao fomulario de logout a todos
+                .permitAll();                   //Permite acesso ao fomulario de logout a todos
     }
     
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {     //verifica os logins
+        UserDetails user = User.withDefaultPasswordEncoder()
+                            .username("ramon")
+                            .password("ramon")
+                            .roles("USER")
+                            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 }
